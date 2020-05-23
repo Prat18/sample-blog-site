6f05683d1a2a0b2dcf0e4c8e5dc2202fea6b4799
pratyush kumar <pratyush181996@gmail.com>
2020-05-23 05:56:09 +0530

    UI progress

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core'; // importing Component from node_modules/angular/core.
-
+import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
 //Component Decorator is used to make angular understand, this class is component.
 //Component decorator takes some configuration in the form of a javascript object which we pass to it
 //in that object we need to define things like the template, selector, .
@@ -11,14 +13,21 @@ import { Component } from '@angular/core'; // importing Component from node_modu
 //creating component
 export class PostCreateComponent {
   //Declaring var/property
-  enteredValue = '';
-  newPost = 'CONTEXT';
   //Adding method which is used in html file.
   //onAddPost(inputPost: HTMLTextAreaElement) {
     //this.newPost = inputPost.value;
     //alert('Post added!');
   //}
-  onAddPost() {
-    this.newPost = this.enteredValue;
+  //enteredTitle = '';
+  //enteredContent = '';
+  //@Output()
+  //postCreated = new EventEmitter <Post>();
+
+  constructor(public postsService: PostsService) {}
+
+  onAddPost(form: NgForm) {
+    if(form.invalid) return;
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
-  templateUrl: './post-list.component.html'
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
+  /* posts = [
+    {title: 'First post', content: 'This is the first post.'},
+    {title: 'Second post', content: 'This is the second post.'},
+    {title: 'Third post', content: 'This is the Third post.'}
+  ]; */
+  //@Input()
+  posts: Post[] = [];
+  private postSub = new Subscription;
 
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPost();
+    this.postSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postSub.unsubscribe();
+  }
 }
